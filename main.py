1afcdc47d072f1917f43af552bbae283299f38ad
Kunal Chauhan <Kunal.Chauhan@bain.com>
2023-06-13 12:27:27 +0530

    KB Refactoring code and created first 4 f

--- main.py
+++ main.py
@@ -8,65 +8,85 @@ load_dotenv()
 songsName = ['abc.mp3', 'xyz ahs 8uj - .mp3']
 spotifySongURI = []
 
+def populateSongs():
+    # # Address to fetch the files
+    # # path = "C://Users//Vanshi//Desktop//gfg"
+    # try:
+    #     # songsName = os.listdir(path)
+    # except:
+    #     print("Unable to fetch songs from the given destination")
+    return
 
-# Address to fetch the files
-# path = "C://Users//Vanshi//Desktop//gfg"
-# songsName = os.listdir(path)
+def processSongsList():
+    # loop to fetch only the song names (splitting the mp3 extension)
+    for i in range(len(songsName)):
+        songsName[i] = songsName[i].split('.')[0]
+    
+    return
 
-# loop to fetch only the song names (splitting the mp3 extension)
-for i in range(len(songsName)):
-    songsName[i] = songsName[i].split('.')[0]
+def retrieveAccessToken():
+    # Calling API for access Token
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
 
-# Calling API for access Token
-headers = {
-    'Content-Type': 'application/x-www-form-urlencoded'
-}
-data = "grant_type=client_credentials&client_id=" + \
-    os.getenv('client_id')+"&client_secret="+os.getenv('client_secret')
+    data = "grant_type=client_credentials&client_id=" + \
+        os.getenv('client_id')+"&client_secret="+os.getenv('client_secret')
 
-try:
-    accessTokenJson = requests.post(
-        'https://accounts.spotify.com/api/token',
-        headers=headers,
-        data=data)
+    try:
+        accessTokenJson = requests.post(
+            'https://accounts.spotify.com/api/token',
+            headers=headers,
+            data=data)
 
-    if (accessTokenJson.status_code != 200):
-        print("API status not 200 for access Token. The response code is " +
-              accessTokenJson.status_code)
+        if (accessTokenJson.status_code != 200):
+            print("API status not 200 for access Token. The response code is " +
+                accessTokenJson.status_code)
+            quit()
+
+        accessTokenJson = accessTokenJson.json()
+
+    except:
+        print("Error calling the API for access Token")
         quit()
 
-    accessTokenJson = accessTokenJson.json()
+    accessToken = accessTokenJson["access_token"]
 
-except:
-    print("Error calling the API for access Token")
-    quit()
+    # print(accessToken)
+    return accessToken
 
-accessToken = accessTokenJson["access_token"]
-# print(accessToken)
+def callAPItoSearchPopulateSongsURI(accessToken):
+    # API call
+    headers = {
+        'Authorization': 'Bearer ' + accessToken}
 
-# API call
-headers = {
-    'Authorization': 'Bearer ' + accessToken}
+    try:
+        response = requests.get('https://api.spotify.com/v1/search?' +
+                                'q=dil+kyu+yeah+mera' +
+                                '&type=track&limit=1',
+                                headers=headers
+                                )
 
-try:
-    response = requests.get('https://api.spotify.com/v1/search?' +
-                            'q=dil+kyu+yeah+mera' +
-                            '&type=track&limit=1',
-                            headers=headers
-                            )
+        if (response.status_code != 200):
+            print("API status not 200 for track search. The response code is " +
+                response.status_code)
+            quit()
 
-    if (response.status_code != 200):
-        print("API status not 200 for track search. The response code is " +
-              response.status_code)
+        response = response.json()
+
+    except:
+        print("Error calling the API for track search")
         quit()
 
-    response = response.json()
+    spotifySongURI.append(response['tracks']['items'][0]['uri'])
 
-except:
-    print("Error calling the API for track search")
-    quit()
+    # print(spotifySongURI)
+    return 
 
-spotifySongURI.append(response['tracks']['items'][0]['uri'])
+populateSongs()
+processSongsList()
+accessToken =  retrieveAccessToken()
+callAPItoSearchPopulateSongsURI(accessToken)
 
-print(spotifySongURI)
+#print(spotifySongURI)
 # print(songsName)
